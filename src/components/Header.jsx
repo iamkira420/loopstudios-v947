a188e581c360782e2fba1330b2bc6d1881f41aa4
Lutho420 <lmboniswa01@gmail.com>
2022-11-23 01:49:39 +0200

    git commit -m "chore: add hamburg menu+header txt"

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-
+import './css/Header.css';
+import background from './images/desktop/image-hero.jpg';
+import hamburger from './images/icon-hamburger.svg';
 
 export default function Header() {
     return (
-        <header className="header">
+        <header className="header" style={{ backgroundImage: `url(${background})` }}>
             <nav className="navbar">
-                <h1>loopstudio</h1>
+                <a className="loopstudio" href="/">
+                    <h1>
+                        loopstudio
+                    </h1>
+                </a>
                 <ul className="navbar-list">
                     <li><a href="" >About</a></li>
                     <li><a href="" >Careers</a></li>
@@ -13,6 +19,7 @@ export default function Header() {
                     <li><a href="" >Products</a></li>
                     <li><a href="" >Support</a></li>
                 </ul>
+                <img className="hamburger" src={hamburger} alt="" />
             </nav>
             <div className="header-line">
                 <h1>Immersive experiences that deliver</h1>
