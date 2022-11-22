25a4e58ab7fc747f6975806c9c6954aa3f1faf23
Lutho420 <lmboniswa01@gmail.com>
2022-11-23 00:40:58 +0200

    git commit -m "build: header component setup"

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+export default function Header() {
+    return (
+        <header className="header">
+            <nav className="navbar">
+                <h1>loopstudio</h1>
+                <ul className="navbar-list">
+                    <li><a href="" >About</a></li>
+                    <li><a href="" >Careers</a></li>
+                    <li><a href="" >Events</a></li>
+                    <li><a href="" >Products</a></li>
+                    <li><a href="" >Support</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
